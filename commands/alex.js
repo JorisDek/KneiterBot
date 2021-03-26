f07724e74418ff38a484b874be60589f7125a86c
Joris Dek <jdek@thirdwave.nl>
2021-03-26 10:19:07 +0100

    Added command

---
+++ commands/alex.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'alex',
+    description: 'What does the bot say when you say "alex"',
+    execute(message, args) {
+        message.channel.send('Alex de palex de pornoster!')
+    }
+}
