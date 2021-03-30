82c6472ea2f2fed9db23d244c15d11de5cb76d8c
Joris Dek <jdek@thirdwave.nl>
2021-03-30 17:06:21 +0200

    bug fixes

--- commands/deposit.js
+++ commands/deposit.js
@@ -11,17 +11,20 @@ module.exports = {
         if(amount % 1 != 0 || amount <= 0) return message.channel.send('Deposit must be a whole number above 0')
         try {
             if (amount > userData.coins) return message.channel.send("You don't have that much coins")
+            
+            let interest = amount * 0.02
+            
 
             await profileModel.findOneAndUpdate({
                 userID: message.author.id
             }, {
                 $inc: {
                     coins: -amount,
-                    bank: amount
+                    bank: amount-interest
                 }
             })
 
-            return message.channel.send(`You depositted ${amount} coins into you bank.`)
+            return message.channel.send(`You deposited ${amount-interest} coins into your bank. Interest = 2%`)
 
         } catch(err) {
             console.error(err)
